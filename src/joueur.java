dcef2f6b0c3c16b2f126b8cfc9ff5c5473fd320c
Adrien <Adrien@DESKTOP-CE3FBT2.home>
2017-04-30 04:22:23 +0200

    MAP finie

--- src/joueur.java
+++ src/joueur.java
@@ -8,34 +8,49 @@ public class joueur {
 		StdDraw.setYscale(0,17);
 		for (int x=0;x<21;x++){
 			for(int y=0;y<17;y++){
-				if (x==0 || y==0 || x==20 || y==16){ //On créé le contour
-					StdDraw.setPenColor(StdDraw.BLACK);
-					int type_objet = 1; //Type = 1 => Mur Indestructible
+				if (x%2 == 1 || y%2 == 1){ //On créé le contour
+					StdDraw.setPenColor(StdDraw.ORANGE);
+					String type_objet = "breakable"; //Type = 1 => Mur Indestructible
 				}
-				else if (x==1 || y==1 || x==19 || y==15){//On créé le chemin de ronde
-					StdDraw.setPenColor(StdDraw.YELLOW);
-					int type_objet = 2; // Type = 2 => Chemin
+				
+				else{
+					StdDraw.setPenColor(StdDraw.GRAY);
+					String type_objet = "unbreakable";
 				}
-				else if(x<20 && y<16){ //On rentre dans le carré de jeu
-					if (x%2 == 0 || y%2==0){
-						StdDraw.setPenColor(StdDraw.BLACK);
-						int type_objet = 1; 
-					}
-				}
+				if (x==0 || y==0 || x == 20 || y == 16){
+					StdDraw.setPenColor(StdDraw.GRAY);
+					String type_objet = "border";
+				}	 
+					
 				StdDraw.filledSquare(x+0.5,y+0.5,0.5);
-				/*while (x>2 && y>2 && x<20 && y<10){
-					if (x%2==1 && y%2 == 1){
-						StdDraw.setPenColor(StdDraw.BLACK);
-						int type_objet = 1;
-					}*/
-
-	
 				
-				
-				
-				
-			}
+		}
+			
+	}
+		breakblock(19,15); // On créé des chemins autour de joueurs (haut droite)
+		breakblock(18,15);
+		breakblock(17,15);
+		breakblock(19,14);
+		breakblock(19,13);
+		breakblock(18,13);
+		breakblock(17,14);
+		//bas gauche
+		breakblock(1,1); // On créé des chemins autour de joueurs (bas gauche)
+		breakblock(2,1);
+		breakblock(3,1);
+		breakblock(1,2);
+		breakblock(1,3);
+		breakblock(3,2);
+		breakblock(2,3);
+		
+		
+}
+	public static void breakblock (int x,int y){
+			StdDraw.setPenColor(StdDraw.GREEN);
+			StdDraw.filledSquare(x+0.5,y+0.5,0.5);
+			String type_objet = "path";
 		}
 	}
 
-}
+
+
